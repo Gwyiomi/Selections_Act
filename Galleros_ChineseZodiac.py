690bbbde83b563e9136c6062cb86e678388501e4
Gwyiomi <145018234+Gwyiomi@users.noreply.github.com>
2023-10-15 18:42:26 +0800

    Add files via upload

---
+++ Galleros_ChineseZodiac.py
@@ -0,0 +1,86 @@
+year = input("Whater is your Birth year:")
+
+rat = 1900
+ox = 1901
+tiger = 1902
+rabbit = 1903
+dragon = 1904
+snake = 1905
+horse = 1906
+sheep = 1907
+monkey = 1908
+rooster = 1909
+dog = 1910
+pig = 1911
+
+while rat <= 2103 :
+    if year == str(rat):
+        print("Your Chinese zodiac sign is Rat.")
+        break
+    rat += 12
+
+    if year == str(ox):
+        print("Your Chinese zodiac sign is Ox.")
+        break
+    ox += 12
+
+    if year == str(tiger):
+        print("Your Chinese zodiac sign is Tiger.")
+        break
+    tiger += 12
+
+    if year == str(rabbit):
+        print("Your Chinese zodiac sign is Rabbit.")
+        break
+    rabbit += 12
+
+    if year == str(dragon):
+        print("Your Chinese zodiac sign is Dragon.")
+        break
+    dragon += 12
+
+    if year == str(snake):
+        print("Your Chinese zodiac sign is Snake.")
+        break
+    snake += 12
+
+    if year == str(horse):
+        print("Your Chinese zodiac sign is Horse.")
+        break
+    horse += 12
+
+    if year == str(sheep):
+        print("Your Chinese zodiac sign is Sheep.")
+        break
+    sheep += 12
+
+    if year == str(monkey):
+        print("Your Chinese zodiac sign is Monkey.")
+        break
+    monkey += 12
+
+    if year == str(rooster):
+        print("Your Chinese zodiac sign is Rooster.")
+        break
+    rooster += 12
+
+    if year == str(dog):
+        print("Your Chinese zodiac sign is Dog.")
+        break
+    dog += 12
+
+    if year == str(pig):
+        print("Your Chinese zodiac sign is Pig.")
+        break
+    pig += 12
+
+else:
+    print("Invalid Input")
+
+
+
+
+
+
+
+
